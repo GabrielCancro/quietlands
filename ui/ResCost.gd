32a9bd3907d8728fc96a82fc9b7d91a209c9409b
gabrielcancro <gabrielcancro@3ogs.com.ar>
2023-08-22 12:20:37 -0300

    update

---
+++ ui/ResCost.gd
@@ -0,0 +1,27 @@
+extends Control
+
+var cost = {"f":3,"w":2,"s":0}
+func _ready():
+	set_cost(cost)
+
+func set_cost(_cost):
+	cost = _cost
+	print("COST ",cost)
+	var Btns = $HBox.get_children()
+	var index = 0
+	for b in Btns: b.visible = false
+	for c in cost:
+		var internal=0
+		for i in range(cost[c]):
+			Btns[index].visible = true
+			if c=="f": Btns[index].get_node("Sprite").frame = 0
+			if c=="w": Btns[index].get_node("Sprite").frame = 1
+			if c=="s": Btns[index].get_node("Sprite").frame = 2
+			if GC.RES[c]>internal: Btns[index].modulate = Color(1,1,1,1)
+			else: Btns[index].modulate = Color(1,0,0,1)
+			index += 1
+			internal += 1
+		Btns[index].visible = true
+		Btns[index].get_node("Sprite").frame = 5
+		index += 1
+	Btns[index].visible = false
--- ui/ResPopup.gd
+++ ui/ResPopup.gd
@@ -10,6 +10,7 @@ func set_popup(node):
 	if node && "interaction_name" in node && node.interaction_name!=null:
 		$Name.text = node.interaction_name
 		$Cost.bbcode_text = GC.get_bbcode_cost(node.interaction_cost)
+		$ResCost.set_cost(node.interaction_cost)
 		$Tween.interpolate_property(self,"modulate",modulate,Color(1,1,1,1),.2)
 	else: 
 		$Tween.interpolate_property(self,"modulate",modulate,Color(0,0,0,0),.2)
