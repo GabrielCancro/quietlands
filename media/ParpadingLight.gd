4841708fb1a9d6941329f81856d0dcafb378c597
Gabriel <gabrielcancro@gmail.com>
2024-08-07 12:09:21 -0300

    update

--- media/ParpadingLight.gd
+++ media/ParpadingLight.gd
@@ -1,5 +1,6 @@
 extends Light2D
 
+export var amplitude = 0.05
 onready var b_scale = scale 
 onready var b_energy = energy 
 
@@ -11,8 +12,8 @@ func _process(delta):
 	color.a = get_parent().modulate.a
 
 func light_animation():
-	var to_scale = b_scale*Vector2(rand_range(.95,1.05), rand_range(.95,1.05) )
-	var to_energy = b_energy*rand_range(0.95,1.05)
+	var to_scale = b_scale*Vector2(rand_range(1-amplitude,1+amplitude), rand_range(1-amplitude,1+amplitude) )
+	var to_energy = b_energy*rand_range(1-amplitude,1+amplitude)
 	$Tween.remove_all()
 	$Tween.interpolate_property(self,"scale",scale,to_scale,.08,Tween.TRANS_QUAD,Tween.EASE_OUT_IN)
 	$Tween.interpolate_property(self,"energy",energy,to_energy,.08,Tween.TRANS_QUAD,Tween.EASE_OUT_IN)
---
+++ scenes/Map.gd
@@ -0,0 +1,32 @@
+extends Control
+
+var levels = {
+	"L1":"winned",
+	"L2":"toPlay",
+	"L3":"toPlay",
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_level_states()
+
+func set_level_states():
+	for lv_node in $Levels.get_children():
+		(lv_node as Button).focus_mode = Button.FOCUS_NONE
+		(lv_node as Button).connect("button_down",self,"on_click",[lv_node])
+		var index = lv_node.get_index()+1
+		var name = "L"+str(index)
+		var light = (lv_node.get_node("LIGHT1") as Light2D)
+		var sprite = (lv_node.get_node("Sprite") as Sprite)
+		sprite.texture = null
+		light.color = Color(.01,.01,.01,1)
+		if name in levels.keys():
+			if levels[name] == "toPlay":
+				sprite.texture = preload("res://assets/Elements/obj_dark_cristal_small.png")
+				light.color = Color(1,0,0,1)
+			if levels[name] == "winned":
+				sprite.texture = preload("res://assets/Elements/bld_house_2.png")
+				light.color = Color(.5,.5,.5,1)
+
+func on_click(lv_node):
+	print("CLICK ON ",lv_node.name)
